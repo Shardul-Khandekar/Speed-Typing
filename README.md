e970f1779b737a3691ebacbd45a1b18b17060b69
unknown <khandekarshardul@gmail.com>
2024-06-17 23:15:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Speed Typing
+
+NOTE: Recreation of already existing [monkeytype](https://monkeytype.com)
+
+## How to run locally
+
+```zsh
+git clone https://github.com/salmannotkhan/typing-test.git
+cd typing-test
+npm install
+npm start
+npm run build
+```
+
---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { State } from "store/reducer";
+import "stylesheets/Footer.scss";
+
+export default function Footer() {
+    const { timerId } = useSelector((state: State) => state.time);
+
+    return (
+        <div className={`bottom-area ${timerId ? "hidden" : ""}`}>
+            <span className="hint">
+                <kbd>Ctrl</kbd> + <kbd>k</kbd> to open command pallet
+            </span>
+            <span className="hint">
+                <kbd>Tab</kbd> to restart test
+            </span>
+            <footer>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="">
+                    <span>&lt;/&gt;</span> github
+                </a>
+            </footer>
+        </div>
+    );
+}
